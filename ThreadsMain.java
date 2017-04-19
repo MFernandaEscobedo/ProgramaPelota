8b33122a93fff0be5beb9aa7ecabab34b52e4c03
MFernandaEscobedo <mfernanda.escobedo@hotmail.com>
2017-04-19 16:46:50 -0500

    Create ThreadsMain.java

---
+++ ThreadsMain.java
@@ -0,0 +1,16 @@
+
+import Pelota.MarcoRebote;
+import javax.swing.*;
+
+
+public class UsoThreads {
+
+    public static void main(String[] args) {
+
+        JFrame marco = new MarcoRebote();
+
+        marco.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        marco.setVisible(true);
+    }
+}
